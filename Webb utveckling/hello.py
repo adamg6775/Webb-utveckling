a6682fec5f514ed7b10267409faf3018fff1c407
adamg6775 <143708434+adamg6775@users.noreply.github.com>
2024-01-12 13:19:47 +0100

    Add files via upload
    Jag har börjat gjort egna css filer till varje sida så att varje sida har sitt egna färg tema

---
+++ Webb utveckling/hello.py
@@ -0,0 +1,15 @@
+print ("hello python!")
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Greet the user by name
+print(f"Hello, {name} ")
+
+# Lesson: Conditional Statements
+age = int(input("Enter your age: "))
+if age < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
+
+    
